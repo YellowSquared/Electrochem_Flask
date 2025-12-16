0140d2bd8dbacb89e7533202d333ec03134e3dc1
Безрукавый Андрей <130885057+YellowSquared@users.noreply.github.com>
2025-12-17 00:15:59 +0500

    Удобный запуск

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-db = SQLAlchemy()
-from app.models import *
 
 
+load_dotenv()
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-
-# with app.app_context():
-#     db.create_all()
-
-
-from .routes import main
-app.register_blueprint(main)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app.models import *
